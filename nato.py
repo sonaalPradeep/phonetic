e60aef6cd20dd5dad502bc3795d8c33d0aad4729
Sonaal P. Pradeep <kingoftheland28@gmail.com>
2019-12-09 19:13:04 +0530

    modification for load_dict

--- nato.py
+++ nato.py
@@ -1,4 +1,6 @@
-def nato(in_str):
+import load_dict
+
+def nato(phonetic_dict, in_str):
 	'''
 	This function can be used to convert a sentence into its NATO equivalent
 	The sentence can contain alphanumeric characters or symbols, but the output will ignore the symbols
@@ -7,45 +9,6 @@ def nato(in_str):
 	Returns -- A response string which spells the sentence and a list which consists of translations of
 	each word
 	'''
-
-	phonetic_dict = {
-	"A" : "ALPHA",
-	"B" : "BRAVO",
-	"C" : "CHARLIE",
-	"D" : "DELTA",
-	"E" : "ECHO",
-	"F" : "FOXTROT",
-	"G" : "GOLF",
-	"H" : "HOTEL",
-	"I" : "INDIA",
-	"J" : "JULIETT",
-	"K" : "KILO",
-	"L" : "LIMA",
-	"M" : "MIKE",
-	"N" : "NOVEMBER",
-	"O" : "OSCAR",
-	"P" : "PAPA",
-	"Q" : "QUEBEC",
-	"R" : "ROMEO",
-	"S" : "SIERRA",
-	"T" : "TANGO",
-	"U" : "UNIFORM",
-	"V" : "VICTOR",
-	"W" : "WHISKEY",
-	"X" : "XRAY",
-	"Y" : "YANKEE",
-	"Z" : "ZULU",
-	"1" : "ONE",
-	"2" : "TWO",
-	"3" : "THREE",
-	"4" : "FOUR",
-	"5" : "FIVE",
-	"6" : "SIX",
-	"7" : "SEVEN",
-	"8" : "EIGHT",
-	"9" : "NINER",
-	"0" : "ZERO",
-	}
 
 	in_str = in_str.upper()
 	words = in_str.split()
@@ -71,21 +34,23 @@ def nato(in_str):
 
 if __name__ == "__main__":
 	# The nato(in_str) function can be called as follows :
-	output_string, output_list = nato("Hello World")
+	ld_dict = load_dict.load_dict()
+
+	output_string, output_list = nato(ld_dict, "Hello World")
 	print("Input string : {}".format("Hello World"))
 	print("NATO string : {}".format(output_string))
 	print("NATO list : {}".format(output_list))
 	print()
 
 	# The same function can be called with numbers as its parameters
-	output_string, output_list = nato("123467890")
+	output_string, output_list = nato(ld_dict, "123467890")
 	print("Input string : {}".format("123467890"))
 	print("NATO string : {}".format(output_string))
 	print("NATO list : {}".format(output_list))
 	print()
 
 	# Note that spaces are used for sparsing words and all other symbols are ignored
-	output_string, output_list = nato("!@#$% ^&*()")
+	output_string, output_list = nato(ld_dict, "!@#$% ^&*()")
 	print("Input string : {}".format("!@#$% ^&*()"))
 	print("NATO string : {}".format(output_string))
 	print("NATO list : {}".format(output_list))
